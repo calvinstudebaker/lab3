8094ef305f0cf4cb406d7dae01712ff30d3f981b
Calvin Studebaker <calstud@stanford.edu>
2014-01-23 07:22:44 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,45 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("CHANGE");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
+
+	$("a.thumbnail").click(projectClick);
+
+	//Changing projects with the html form
+	$("#submitBtn").click(changeProject);
+};
+
+function changeProject(e){
+	var project = $("#project").val();
+	var newWidth = $("#width").val();
+	var newDescription = $("#description").val();
+
+	$(project).animate({
+		width: newWidth
+	});
+	var thisDescription = project + " .project-description";
+	$(thisDescription).text(newDescription);
+};
+
+function projectClick(e){
+	console.log("Project Clicked");
+
+	e.preventDefault();
+
+	var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+       description.hide('slow');
+    }
+};
